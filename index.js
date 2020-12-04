e568eb9ce94f1b1f629d36732e89f0eb24eabf77
danilmuis <daniltelkom@gmail.com>
2020-12-04 10:18:08 +0700

    Update gan

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const hbs = require('hbs')
-const multer = require('multer')
-const upload = multer()
+const multer = require('multer');
+const upload = multer();
 const {Trie} = require('regexgen');
 
 app.set('views',path.join(__dirname,'views'))
